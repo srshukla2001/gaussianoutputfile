53e969751a491b6c5d90c19be90fbee0337a9e82
srshukla2001 <129613229+srshukla2001@users.noreply.github.com>
2025-08-27 16:25:31 +0530

    nodemon

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
-import * as GaussianSplats3D from '@mkkellogg/gaussian-splats-3d';
-import * as THREE from 'three';
 
-fetch('scene.json').then(r=>r.json()).then(data=>{
-  const viewer=new GaussianSplats3D.Viewer({
-    cameraUp:[0.01933,-0.75830,-0.65161],
-    initialCameraPosition:[1.5,2.6,-6.3],
-    initialCameraLookAt:[0.45,1.95,1.5]
-  });
-  viewer.addSplatScene(data.splatPath,{progressiveLoad:false}).then(()=>{
-    viewer.start();
-    const scene=viewer.threeScene;
+import * as GaussianSplats3D from '/lib/gaussian-splats-3d.module.js';
+import * as THREE from '/lib/three.module.js';
+import { GLTFLoader } from '/lib/GLTFLoader.js';
 
-    const box=new THREE.Mesh(
-      new THREE.BoxGeometry(1,1,1),
-      new THREE.MeshStandardMaterial({color:'#'+data.box.color})
-    );
-    box.position.fromArray(data.box.position);
-    box.rotation.set(...data.box.rotation);
-    box.scale.setScalar(data.box.scale);
+const loader = new GLTFLoader();
 
-    scene.add(new THREE.AmbientLight(0x404040,1.5));
-    const dir=new THREE.DirectionalLight(0xffffff,1);
-    dir.position.set(5,10,7);
-    scene.add(dir);
-    scene.add(box);
-  });
-});
+fetch('scene.json')
+  .then(res => res.json())
+  .then(data => {
+    const viewer = new GaussianSplats3D.Viewer({
+      cameraUp: [0.01933, -0.7583, -0.65161],
+      initialCameraPosition: [1.5, 2.6, -6.3],
+      initialCameraLookAt: [0.45, 1.95, 1.5]
+    });
+
+    viewer.addSplatScene(data.splatPath, { progressiveLoad: false }).then(() => {
+      viewer.start();
+      const scene = viewer.threeScene;
+
+      // Lighting
+      scene.add(new THREE.AmbientLight(0x404040, 1.5));
+      const dir = new THREE.DirectionalLight(0xffffff, 1);
+      dir.position.set(5,10,7);
+      scene.add(dir);
+
+      data.models.forEach(model => {
+        if(model.name.toLowerCase().endsWith('.glb')) {
+          loader.load('assets/' + model.name, gltf => {
+            const obj = gltf.scene;
+            obj.name = model.name;
+            if(model.position) obj.position.fromArray(model.position);
+            if(model.rotation) obj.rotation.set(...model.rotation);
+            if(model.scale) obj.scale.setScalar(model.scale);
+            scene.add(obj);
+          });
+        } else {
+          let geometry;
+          switch(model.geometryType) {
+            case 'BoxGeometry': geometry = new THREE.BoxGeometry(1,1,1); break;
+            case 'TrapeziumGeometry': geometry = new THREE.CylinderGeometry(0.5,1,1,4); break;
+            case 'CylinderGeometry': geometry = new THREE.CylinderGeometry(0.5,0.5,1,16); break;
+            default: geometry = new THREE.BoxGeometry(1,1,1);
+          }
+
+          const material = new THREE.MeshStandardMaterial({
+            color: model.color ? '#' + model.color : '#ffff00',
+            wireframe: model.wireframe || false
+          });
+
+          const obj = new THREE.Mesh(geometry, material);
+          if(model.position) obj.position.fromArray(model.position);
+          if(model.rotation) obj.rotation.set(...model.rotation);
+          if(model.scale) obj.scale.setScalar(model.scale);
+          obj.name = model.name;
+          scene.add(obj);
+        }
+      });
+    });
+  })
+  .catch(err => console.error('Failed to load scene.json', err));
